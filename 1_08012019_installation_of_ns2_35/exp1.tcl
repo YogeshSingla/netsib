52ffb2b136beb2da9235f1c5ff70b4ebea312d87
kirito_ys <yogeshsingla131@gmail.com>
2019-01-08 14:37:40 +0530

    Add files via upload

---
+++ 1_08012019_installation_of_ns2_35/exp1.tcl
@@ -0,0 +1,33 @@
+set ns [new Simulator]
+
+#all events will be traced in that file
+set tr [open out.tr w]
+$ns trace-all $tr
+
+#nam file will be used in visualisation
+set namtr [open out.nam w]
+$ns namtrace-all $namtr
+
+#create 5 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+#droptail is a type of queue
+$ns duplex-link $n0 $n1 10Mb 5ms DropTail
+$ns duplex-link $n2 $n0 10Mb 5ms DropTail
+$ns duplex-link $n3 $n0 10Mb 5ms DropTail
+$ns duplex-link $n4 $n1 10Mb 5ms DropTail
+$ns duplex-link $n5 $n1 10Mb 5ms DropTail
+
+$ns duplex-link-op $n0 $n1 orient right
+$ns duplex-link-op $n0 $n2 orient left-up
+$ns duplex-link-op $n0 $n3 orient left-down
+$ns duplex-link-op $n1 $n4 orient right-up
+$ns duplex-link-op $n1 $n5 orient right-down
+
+$ns at 50.0 "$ns halt"
+$ns run
