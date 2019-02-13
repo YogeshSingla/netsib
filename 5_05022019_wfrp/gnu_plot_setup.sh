0553d20e7d789edcb40985fb922db99d76e65d13
kirito_ys <yogeshsingla131@gmail.com>
2019-02-13 11:30:53 +0530

    dt code

---
+++ 5_05022019_wfrp/gnu_plot_setup.sh
@@ -0,0 +1,4 @@
+#steps to install gnuplot
+#to run the shell scrip type sudo sh <file_name>.sh (here gnu_plot_setup.sh) in command line. Provide root password if required
+sudo apt-get update
+sudo apt-get install gnuplot-x11
