14522b3cf015d4909eda22ac60875214a124fda4
kirito_ys <yogeshsingla131@gmail.com>
2019-02-19 16:08:50 +0530

    Add files via upload

--- 7_19022019/exp1.tcl
+++ 7_19022019/exp1.tcl
@@ -1,18 +1,29 @@
+if {$argc != 1} {
+       error "\nCommand: ns exp1.tcl <no.of.nodes>\n\n "
+}
+
 set val(chan)           Channel/WirelessChannel    ;# channel type
-set val(prop)           Propagation/TwoRayGround   ;# radio-propagation model
+#set val(prop)           Propagation/TwoRayGround   ;# radio-propagation model
+Propagation/Shadowing set pathlossExp_ 3.5  ;# path loss exponent
+Propagation/Shadowing set std_db_ 4.0       ;# shadowing deviation (dB)
+Propagation/Shadowing set dist0_ 0.5        ;# reference distance (m)
+Propagation/Shadowing set seed_ 0           ;# seed for RNG
+set val(prop)           Propagation/Shadowing   ;# radio-propagation model
+Phy/WirelessPhy set CSThresh_ 1.92278e-6 ;#10m
+Phy/WirelessPhy set RXThresh_ 1.92278e-6 ;#10m
 set val(netif)          Phy/WirelessPhy/802_15_4            ;# network interface type
-set val(mac)            Mac/802_15_4                 ;# MAC type
+set val(mac)            Mac/802_15_4              ;# MAC type
 set val(ifq)            Queue/DropTail/PriQueue    ;# interface queue type
 set val(ll)             LL                         ;# link layer type
 set val(ant)            Antenna/OmniAntenna        ;# antenna model
 set val(ifqlen)         50                         ;# max packet in ifq
-set val(nn)             49                        ;# number of mobilenodes
+set val(nn)             [lindex $argv 0]                         ;# number of mobilenodes
 set val(rp)             AODV                       ;# routing protocol
 
 # Initialize Global Variables
 set ns_		[new Simulator]
 $ns_ use-newtrace
-set tracefd     [open out.tr w]
+set tracefd     [open [lindex $argv 0]_out.tr w]
 $ns_ trace-all $tracefd
 set namfile [open out.nam w]
 $ns_ namtrace-all-wireless $namfile 20 20 
@@ -34,58 +45,41 @@ $ns_ node-config -adhocRouting $val(rp) \
 	-topoInstance $topo \
     -energyModel "EnergyModel" \
         -initialEnergy 3.4 \
-        -txPower 0.33 \
-        -rxPower 0.1 \
-        -idlePower 0.05 \
-        -sleepPower 0.03 \
+        -txPower 31.32e-3 \
+        -rxPower 35.28e-3 \
+        -idlePower 712e-6 \
+        -sleepPower 144e-9 \
 	-agentTrace ON \
 	-routerTrace ON \
 	-macTrace OFF \
 	-movementTrace OFF			
 			 
-for {set i 0} {$i < $val(nn) } {incr i} {
+for {set i 0} {$i < $val(nn)} {incr i} {
 		set node_($i) [$ns_ node]	
 		$node_($i) random-motion 1		;# disable random motion
 }
 
-#place the ten nodes
-for {set i 2} {$i < $val(nn) } { incr i } {
-    set xx [expr rand()*20]
-    set yy [expr rand()*20]
+set distance_between_nodes 3
+set epi 0.00000000001
+for {set i 1} {$i < $val(nn) } { incr i } {
+    set yy [expr ($i/7)*$distance_between_nodes + $epi]
+    set xx [expr ($i%7)*$distance_between_nodes + $epi]
+
     $node_($i) set X_ $xx
     $node_($i) set Y_ $yy
     $node_($i) set Z_ 0.0
 
     $ns_ at 0.0 "$node_($i) setdest $xx $yy 0.0"
 }
+
 $node_(0) set X_ 0.0
 $node_(0) set Y_ 0.0
 $node_(0) set Z_ 0.0
 $ns_ at 0.0 "$node_(0) setdest $xx $yy 0.0"
-$node_(1) set X_ 15.0
-$node_(1) set Y_ 15.0
-$node_(1) set Z_ 0.0
-$ns_ at 0.0 "$node_(1) setdest $xx $yy 0.0"
-
-# Setup traffic flow between nodes
-# TCP connections between node_(0) and node_(9)
-# set tcp [new Agent/TCP]
-# $tcp set class_ 2
-# set sink [new Agent/TCPSink]
-# $ns_ attach-agent $node_(0) $tcp
-# $ns_ attach-agent $node_(2) $tcp
-# $ns_ attach-agent $node_(3) $tcp
-# $ns_ attach-agent $node_(1) $sink
-# $ns_ connect $tcp $sink
-# set ftp [new Application/FTP]
-# $ftp attach-agent $tcp
-
-# $ns_ at 1.0 "$ftp start" 
-# $ns_ at 101.0 "$ftp stop" 
 
 #setup a UDP connection
 set null [new Agent/Null]
-$ns_ attach-agent $node_(1) $null
+$ns_ attach-agent $node_(0) $null
 for {set i 2} {$i < $val(nn) } { incr i } {
 	set udp_($i) [new Agent/UDP]
 	$ns_ attach-agent $node_($i) $udp_($i)
@@ -110,10 +104,10 @@ for {set i 2} {$i < $val(nn) } { incr i } {
 
 
 for {set i 0} {$i < $val(nn) } {incr i} {
-    $ns_ at 150.0 "$node_($i) reset";
+    $ns_ at 71.0 "$node_($i) reset";
 }
-$ns_ at 150.0 "stop"
-$ns_ at 150.01 "puts \"NS EXITING...\" ; $ns_ halt"
+$ns_ at 71.0 "stop"
+$ns_ at 71.01 "puts \"NS EXITING...\" ; $ns_ halt"
 
 proc stop {} {
     global ns_ tracefd
@@ -122,7 +116,7 @@ proc stop {} {
     #exec nam out.nam
 }
 
-puts "Starting Simulation..."
+puts "Starting Simulation..." 
 $ns_ run
 puts "Number of nodes: $val(nn)"
 
